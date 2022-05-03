241a0a47b86f93550ee0d2a43b13c1cb2ecd9003
fukenrice <63079551+fukenrice@users.noreply.github.com>
2022-05-03 04:27:33 +0300

    Project view layout added

---
+++ app/src/main/java/com/example/kusashkotlin/ui/main/view/project/Project.kt
@@ -0,0 +1,12 @@
+package com.example.kusashkotlin.ui.main.view.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kusashkotlin.R
+
+class Project : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project)
+    }
+}
